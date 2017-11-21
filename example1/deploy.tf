a81331e2142b14df054560304442edfb9e68333d
Coder <coderdba@users.noreply.github.com>
2017-11-21 09:50:15 +0530

    Create deploy.tf

---
+++ example1/deploy.tf
@@ -0,0 +1,28 @@
+variable "count" {
+  default = 1
+}
+
+resource "openstack_compute_instance_v2" "redis" {
+  count = "${var.count}"
+  name = "${format("redis-%02d", count.index+1)}"
+  image_name = "${var.image}"
+  #availability_zone = "AZ1"
+  #flavor_id = "1"
+  flavor_name = "${var.flavor}"
+  key_pair = "${var.openstack_keypair}"
+  security_groups = ["${var.tenant_security_group}"]
+  network { 
+    #name = "${var.tenant_network}"
+    name = "${var.nw_1}"
+  }
+  user_data = "${file("touch.sh")}"
+}
+
+resource "openstack_networking_floatingip_v2" "fip_1" {
+  pool = "ext_vlan_net"
+}
+
+resource "openstack_compute_floatingip_associate_v2" "fip_1" {
+  floating_ip = "${openstack_networking_floatingip_v2.fip_1.address}"
+  instance_id = "${openstack_compute_instance_v2.redis.id}"
+}
